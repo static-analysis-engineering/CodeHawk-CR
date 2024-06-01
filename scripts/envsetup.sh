e5e82e18aa8b13b73fed2c84d3be7f5c8abb99af
A. Cody Schuffelen <astronomix@gmail.com>
2024-06-01 01:55:30 -0700

    Add environment setup script
    $ source scripts/envsetup.sh
    $ chc_build
    $ chc test

---
+++ scripts/envsetup.sh
@@ -0,0 +1,21 @@
+SOURCED_PATH=$_
+REAL_SOURCE=$(realpath "$SOURCED_PATH")
+SOURCE_DIR=$(dirname "$REAL_SOURCE")
+PROJECT_DIR=$(dirname "$SOURCE_DIR")
+export PATH="$PROJECT_DIR"/chc/cmdline:"$PATH"
+export PYTHONPATH="$PROJECT_DIR"
+if [ ! -d "$PROJECT_DIR"/.venv ]; then
+	python3 -m venv $PROJECT_DIR/.venv
+fi
+source "$PROJECT_DIR"/.venv/bin/activate
+
+chc_build () {
+	cd "$PROJECT_DIR"
+	# Filter emoji out of maturin because they mess up my gnu screen session
+	maturin develop --color=always 2>&1 | tr -cd '\000-\177'
+}
+
+chc_test () {
+	chc_build
+	chkc kendra test-sets
+}
