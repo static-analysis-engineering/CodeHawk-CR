47edd5ced5909e68cab659f6139cef1ad5fc7af9
A. Cody Schuffelen <astronomix@gmail.com>
2024-06-07 23:56:27 -0700

    Add a script function for c-file testing

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -42,3 +42,50 @@ chc_demo () {(
 	chkc c-project analyze ./ codehawk_demo
 	chkc c-project report ./ codehawk_demo
 )}
+
+chc_tables () {(
+	set -e
+	chc_build
+	cd $PROJECT_DIR/tests
+	rm -r demo.build
+	cp -r demo demo.build
+	cd demo.build
+	ninja
+	for FILE in $(ls *.c); do
+		chkc c-file parse $FILE
+		chkc c-file analyze $FILE
+		chkc c-file report $FILE
+		
+		chkc c-file file-table api-param $FILE
+		chkc c-file file-table attrparam $FILE
+		chkc c-file file-table attribute $FILE
+		chkc c-file file-table attributes $FILE
+		chkc c-file file-table cfg-context $FILE
+		# chkc c-file file-table compinfo $FILE # issue with types.c
+		chkc c-file file-table constant $FILE
+		chkc c-file file-table enuminfo $FILE
+		chkc c-file file-table enumitem $FILE
+		chkc c-file file-table exp $FILE
+		chkc c-file file-table exp-context $FILE
+		# chkc c-file file-table fieldinfo $FILE # issue with types.c
+		chkc c-file file-table funarg $FILE
+		chkc c-file file-table funargs $FILE
+		chkc c-file file-table initinfo $FILE
+		chkc c-file file-table lhost $FILE
+		chkc c-file file-table location $FILE
+		chkc c-file file-table lval $FILE
+		chkc c-file file-table offset $FILE
+		chkc c-file file-table offsetinfo $FILE
+		chkc c-file file-table po-predicate $FILE
+		chkc c-file file-table post-assume $FILE
+		chkc c-file file-table post-request $FILE
+		chkc c-file file-table program-context $FILE
+		chkc c-file file-table s-offset $FILE
+		chkc c-file file-table s-term $FILE
+		chkc c-file file-table typeinfo $FILE
+		chkc c-file file-table typsig $FILE
+		chkc c-file file-table typsig-list $FILE
+		chkc c-file file-table varinfo $FILE
+		chkc c-file file-table xpredicate $FILE
+	done
+)}
